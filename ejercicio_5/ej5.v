e3d876f495acba849f0588fc48a133e883450007
j <jmestanza@itba.edu.ar>
2018-08-19 02:17:52 -0300

    discovered how to compare x! :D

--- ejercicio_5/ej5.v
+++ ejercicio_5/ej5.v
@@ -1,23 +1,30 @@
 module main ; // wtf with this ;
 reg aux;
 reg [3:0] in;
-//reg a;
+reg [3:0] somex;
 reg input_err;
 initial begin // begin = {  end = } 
 
 
     assign input_err = 0;
 
-    aux=$value$plusargs("in=%b", in); // in spite of not having "a" value it still executes without any problem
+    aux=$value$plusargs("in=%b", in);
 
-    $display("in's value is:");
+    $display("in's value is:"); 
     $display(in);
+    $display("somex's value is:");
+    $display(somex);
+    
+    begin
+        if(in === somex)
+            assign input_err=1; 
+    end
+
     
     $display("input error is:",input_err);
 
     $finish; //kind of a return 0;
-    // if a valid input is fetched, the value of a is de least significant bit
-
+    
 end
 
 endmodule // End of Module hello_world
