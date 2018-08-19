cfcb9b86123b53bc17d83e0dd2f4f6dcdfdc7a7a
j <jmestanza@itba.edu.ar>
2018-08-19 01:44:49 -0300

    added if example

--- ejercicio_5/ej5.v
+++ ejercicio_5/ej5.v
@@ -1,18 +1,23 @@
 module main ; // wtf with this ;
 reg aux;
-reg a;
+reg [3:0] in;
+//reg a;
+reg input_err;
 initial begin // begin = {  end = } 
 
 
-    $display ("Installation successful!");
+    assign input_err = 0;
 
-    aux=$value$plusargs("a=%b", a); // in spite of not having "a" value it still executes without any problem
+    aux=$value$plusargs("in=%b", in); // in spite of not having "a" value it still executes without any problem
 
-    $display("a's value is:");
-    $display(a);
+    $display("in's value is:");
+    $display(in);
+    
+    $display("input error is:",input_err);
+
     $finish; //kind of a return 0;
     // if a valid input is fetched, the value of a is de least significant bit
-    
+
 end
 
 endmodule // End of Module hello_world
