bffe2fe531eaf7f6eceaf1699eb108194b18a2c4
newtonis <ariel.nowik@gmail.com>
2018-09-05 14:44:02 -0300

    add

---
+++ ejercicio_4/src/program/comp2.v
@@ -0,0 +1,11 @@
+module comp2(d,c,b,a,out_d,out_c,out_b,out_a);
+
+	input d,c,b,a;
+	output out_a,out_b,out_c,out_d;
+
+	assign out_a = a;
+	assign out_b = ((~b) & a) + (b & (~a));
+	assign out_c = (c & (~b) & (~a)) + ((~c) & a) + ((~c) & b & (~a));
+	assign out_d = (d & (~c) & (~b) & (~a)) + ((~d) & c) + ((~d) & (~c) & a) + ((~d) & b & (~a));
+
+endmodule 
---
+++ ejercicio_4/src/tests/comp2_tb.v
@@ -0,0 +1,20 @@
+module comp2_tb();
+
+	reg [3:0] in_test;
+	integer i;
+	wire [3:0] out_test;
+
+	initial begin
+		$monitor("IN: %d %d %d %d - OUT: %d %d %d %d",in_test[3],in_test[2],in_test[1],in_test[0],
+				                              out_test[3],out_test[2],out_test[1],out_test[0]);
+		in_test = 'b0000;
+		for (i = 1;i < 16;i = i+1 ) begin
+		  #1
+		  in_test = in_test + 'b0001;
+		end
+		$finish;
+	end
+
+	comp2 conection(in_test[3],in_test[2],in_test[1],in_test[0],out_test[3],out_test[2],out_test[1],out_test[0]);
+
+endmodule
