ed717363acbf47b0414678b3ff052e9f15e57b21
mmaspero <mmaspero@itba.edu.ar>
2018-09-05 19:40:25 -0300

    add newmux test bench

--- ejercicio_3/src/tests/newdecoder_tb.v
+++ ejercicio_3/src/tests/newdecoder_tb.v
@@ -1,23 +1,25 @@
 module newdecoder_tb;
 
-    reg in1, in2; //decoder inputs for test
-    wire out1, out2, out3, out4; //decoder output
+    parameter k = 3; //number of inputs
 
-    decoder dec (
-        .x1(in1), //input 1
-        .x2(in2), //input 2
-        .a(out1), //output 1
-        .b(out2), //output 2
-        .c(out3), //output 3
-        .d(out4)  //output 4
-        );
+    reg in1; //encoder
+    reg [k-1:0] in2; //decoder inputs for test
+    wire [(2**k)-1:0] out1; //decoder output
+
+
+    newdecoder #(.n(k)) newdec (
+        .en(in1),
+        .in(in2),
+        .out(out1)
+    );
+
 
     integer i, j; 
     initial begin // this generate all possible inputs for a 2-bit input decoder, if another number or a character is introduced, the input value will be the less significant bit of its binary conversion)
 
-        for (j = 0; j < 2; j = j + 1)begin //j has two possible values: 0 or 1
+        for (j = 0; j < 2; j = j + 1)begin //j has two possible values: 0 or 1 (encoder)
 
-            for (i = 0; i < 2; i = i + 1)begin //i has two possible values: 0 or 1
+            for (i = 0; i < 2**k; i = i + 1)begin //i possible values 
                 
                 in1 = j; in2 = i;
 
@@ -33,8 +35,8 @@ module newdecoder_tb;
 
     initial begin
 
-        $monitor (" OUTPUTS a = %b  b = %b  c = %b  d = %b", out1, out2, out3, out4);//displays output 
+        $monitor (" OUTPUTS a = %b", out1);//displays output 
       
     end
 
-    endmodule 
+endmodule 
